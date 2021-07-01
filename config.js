fbb59e04a121887be40183fa61f9e586a3193898
Minh311 <86767723+Minh311@users.noreply.github.com>
2021-07-01 16:08:09 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Mai Anh à !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Em yêu anh có phải không nào 😙',
+    desc: ' Trả lời anh thật lòng nhé !',
+    btnYes: 'Yêu ơi là yêuuu <33',
+    btnNo: 'Anh mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 6h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiii lun <3',
+}
